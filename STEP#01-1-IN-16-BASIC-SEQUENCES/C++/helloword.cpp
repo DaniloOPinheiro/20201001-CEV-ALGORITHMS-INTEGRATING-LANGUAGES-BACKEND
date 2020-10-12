8e61308de9afa07363014df38f806d86b2faa081
Danilo.O.Pinheiro <52793184+DaniloOPinheiro@users.noreply.github.com>
2020-10-12 15:35:10 -0300

    Update helloword.cpp

--- STEP#01-1-IN-16-BASIC-SEQUENCES/C++/helloword.cpp
+++ STEP#01-1-IN-16-BASIC-SEQUENCES/C++/helloword.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-
-//Link de Pesquisa
-//https://www.thepolyglotdeveloper.com/2018/08/building-hello-world-example-cpp-application/
 int main(){
     std::cout << "Hello Word!" << std::endl;
     return 0;
